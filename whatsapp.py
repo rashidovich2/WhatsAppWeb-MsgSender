f7e72aca5d99779f523af5efa455d4727efea9fb
Milan Sony <milanfrom2001@gmail.com>
2022-12-09 21:34:27 +0530

    python code to send whatsapp message to a paticular person has updated

--- whatsapp.py
+++ whatsapp.py
@@ -2,8 +2,12 @@ import pywhatkit as kit
 import pyautogui as pg
 
 try:
-  kit.sendwhatmsg("+917306333244", "hai monee...",13,18)
-  # 1st argument is the number, 2nd: message, 3rd: time in hr and min 24 format (if the time is 1:05 IST then we have to write it as 13:5, no need of adding 0 as prefix to min. Try to give 4,5 min from the present time)
+  kit.sendwhatmsg("+911234567890", "message to send", 20,52) #08:52 IST
+
+  #! 1st argument is the number (try to write the number with country code and without putting space in between the numbers Eg: +911234567890)
+  #! 2nd argument is the message to send
+  #! 3rd argument is the time to send the message in 24hr format (if the time is 1:05 IST then we have to write it as 13,5 no need of adding 0 as prefix to minute. Try to give 4,5 minutes from the present time)
+
   pg.press("enter")
 except:
   print("exception error")
