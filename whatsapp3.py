dae595acf192a8ac454103ebfabd50a92f43e3b7
Sourcery AI <>
2023-09-11 14:22:08 +0000

    'Refactored by Sourcery'

--- whatsapp3.py
+++ whatsapp3.py
@@ -14,7 +14,7 @@ try:
   #! 1st argument is the mobile number (write the mobile number with country code and without putting space in between the numbers Eg: +91XXXXXXXXXX)
   #! 2nd argument is the message to send
   #! 3rd argument is the time to send the message in 24hr format (if the time is 1:05 IST then we have to write it as 13,5 no need of adding 0 as prefix to minute. Try to give 4,5 minutes from the present time)
-  
+
   print("Your 1st message has sent")
 
   time.sleep(2)
@@ -22,7 +22,7 @@ try:
   while count <= limit:
     pg.typewrite(message)
     pg.press("enter")
-    count = count+1
+    count += 1
 
 except:
   print("Exception error")
